d86db7bf1eea4ceed5d8065d78d22bf8cca69562
lizafirstwin <lyxnevich@gmail.com>
2021-04-11 22:44:33 +0300

    feat: add numbers 7 12 13 from task short-track

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,17 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const copyArr = [...names];
+  for (let i = 0; i < copyArr.length - 1; i++) {
+    let conter = 0;
+    for (let j = i + 1; j < copyArr.length; j++) {
+      if (copyArr[i] === copyArr[j]) {
+        copyArr[j] = `${copyArr[j]}(${++conter})`;
+      }
+    }
+  }
+  return copyArr;
 }
 
 module.exports = renameFiles;
